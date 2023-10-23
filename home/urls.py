32aee756eebfa6dbf0f75d4b66ab6d0bbfd10619
ulugbekusmanovdev <uusmanov928@gmail.com>
2023-10-23 13:56:07 +0600

    post by category

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:category_id>/', views.category, name='category')
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published')
+    print(featured_posts)
+    context = {'featured_posts': featured_posts, 'posts': posts}
+    return render(request, 'home.html', context)
+
+
+def category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    category = Category.objects.get(pk=category_id)
+    context = {'posts': posts, 'category': category}
+    return render(request, 'category.html', context)

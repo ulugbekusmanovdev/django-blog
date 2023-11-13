9b02d1735b11b4b92e2c9ba1566b08dd11929ee2
ulugbekusmanovdev <uusmanov928@gmail.com>
2023-11-13 10:51:53 +0600

    add post crud functionality

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from home.models import Category
+from home.models import Category, Blog
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+        exclude = ('slug', 'author', ) #slud author dan boshqasi
